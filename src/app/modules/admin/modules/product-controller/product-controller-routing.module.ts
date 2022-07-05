4ab48f4ed845b7fb2ac6ce62fc3621a780d94c06
KavithRashintha <wgkrashintha@gmail.com>
2022-07-05 20:38:28 +0530

    Product Controller components created

--- src/app/modules/admin/modules/product-controller/product-controller-routing.module.ts
+++ src/app/modules/admin/modules/product-controller/product-controller-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductControllerComponent } from './product-controller.component';
+import {AddProductComponent} from "./components/add-product/add-product.component";
+import {UpdateProductComponent} from "./components/update-product/update-product.component";
+import {DeleteProductComponent} from "./components/delete-product/delete-product.component";
 
-const routes: Routes = [{ path: '', component: ProductControllerComponent }];
+const routes: Routes = [
+  {path: '', redirectTo:'add-product' , pathMatch:'full' },
+  {path:'add-product' , component:AddProductComponent},
+  {path:'update-product' , component:UpdateProductComponent},
+  {path:'delete-product' , component:DeleteProductComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/admin/modules/product-controller/product-controller.module.ts
+++ src/app/modules/admin/modules/product-controller/product-controller.module.ts
@@ -3,15 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { ProductControllerRoutingModule } from './product-controller-routing.module';
 import { ProductControllerComponent } from './product-controller.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { DeleteProductComponent } from './components/delete-product/delete-product.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
-    ProductControllerComponent
+    ProductControllerComponent,
+    AddProductComponent,
+    UpdateProductComponent,
+    DeleteProductComponent
   ],
   imports: [
     CommonModule,
-    ProductControllerRoutingModule
+    ProductControllerRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class ProductControllerModule { }
