988b01828984b74072c30b69e55bfd6430e2e6a5
KavithRashintha <wgkrashintha@gmail.com>
2022-07-06 10:09:14 +0530

    Cart module created

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import {MensComponent} from "./components/mens/mens.component";
+import {WomensComponent} from "./components/womens/womens.component";
+import {BagsComponent} from "./components/bags/bags.component";
+import {GlassesComponent} from "./components/glasses/glasses.component";
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { path: '', redirectTo:'mens-products' , pathMatch:'full' },
+  {path:'mens-products' , component:MensComponent},
+  {path:'womens-products' , component:WomensComponent},
+  {path:'bag-products' , component:BagsComponent},
+  {path:'sunglass-products' , component:GlassesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
